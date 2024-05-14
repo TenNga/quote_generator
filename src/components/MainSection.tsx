3ab5316c0c22a1d615f17a90927fc7b41908b8b4
Kevin Karma <tenngawang6080@gmail.com>
2024-05-14 16:23:19 -0400

    single quote state issue solve, next is to have array of quote in state

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -2,18 +2,37 @@ import React, { useContext, useEffect } from 'react';
 import PrimaryImage from './PrimaryImage';
 import TagInput from './form/TagInput';
 import { QuoteContext } from '../contexts/quoteContext';
+import { useQueryQuote } from '../hooks/useQueryQuote';
+import { QuoteContextType, QuoteType } from '../types';
 
 function MainSection () {
     const { quotes, setQuotes }  = useContext(QuoteContext);
 
-    useEffect(()=>{
-        console.log("quotes:: ",quotes)
-    },[])
-    
+    const { isLoading, error, data } = useQueryQuote();
+
+    setQuotes((prev) => {
+        if(prev) {
+            return [...prev, data?.data[0]]
+        }
+        return data?.data;
+    })
+
+    console.log("QUOTES:: ",quotes)
+
+    const renderQuote = quotes?.map(quote => {
+        return(
+            <div>
+                <h1>{quote.author}</h1>
+                <p>{quote.content}</p>
+            </div>
+        )
+    })
+
     return(
         <div className='flex flex-col items-center justify-center'>
             <TagInput />
             <PrimaryImage />
+            {isLoading? <h4>Loading...</h4> : renderQuote}
         </div>
     )
 };
