f9a63a378c26ecc2906c0834a3aa7d5c1a3df5a8
Kevin Karma <tenngawang6080@gmail.com>
2024-05-15 19:39:34 -0400

    navigation updated

--- src/components/form/SearchInput.tsx
+++ src/components/form/SearchInput.tsx
@@ -25,8 +25,8 @@ function SearchInput() {
     }
     return(
         <form className="mt-10 mx-6 mb-6 flex flex-col justify-center items-center" onSubmit={handleSubmit}>
-            <button type="submit" className="px-8 text-3xl py-2 border rounded-md font-medium">Generate New Quote</button>
-            <input type="text" value={term} placeholder="Enter Tag (Optional)" onChange={handleChange} className="border-b border-neutral-900 mt-4 py-1 text-center" />
+            <button type="submit" className="px-8 text-3xl py-2 border rounded-md font-medium hover:bg-indigo-400 hover:text-white  max-[400px]:text-xl">Generate New Quote</button>
+            <input type="text" value={term} placeholder="Topics (Optional: separate by comma)" onChange={handleChange} className="w-full border-b border-neutral-900 mt-4 py-1 text-center" />
         </form>
     )
 };
--- src/components/navigations/Menu.tsx
+++ src/components/navigations/Menu.tsx
@@ -4,8 +4,8 @@ function Menu() {
     return(
         <div className="mx-6 my-4 text-base">
             <ul className="flex">
-                <li className="mr-6 capitalize">about</li>
-                <li className="mr-6 capitalize">Top Quotes</li>
+                <li className="mr-6 capitalize cursor-pointer"><a href="https://www.kevinkarma.me" target="_blank">Developer</a></li>
+                <li className="mr-6 capitalize cursor-pointer"><a href="https://github.com/lukePeavey/quotable" target="_blank">API</a></li>
             </ul>
         </div>
     )
